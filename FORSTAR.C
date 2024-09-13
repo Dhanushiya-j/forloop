a649702cb8eff2f6237e780b2b0fd0d4fa26975e
Dhanushiya-j <dhanushiyajershilin.2006@gmail.com>
2024-09-13 11:52:28 +0530

    Add files via upload

---
+++ FORSTAR.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int row,i,j;
+clrscr();
+printf("no of rows");
+scanf("%d",&row);
+for(i=1;i<=row;i++)
+{for(j=1;j<=i;j++)
+{printf("*");
+}
+printf("\n");
+}
+getch();
+}
